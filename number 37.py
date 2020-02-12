bbed3a9726e7894cf94ecd6a852b1bc948ae3850
tom3108 <t.madziara@gmail.com>
2020-02-13 00:31:27 +0100

    Python basic exercises. PartI. Number 45-58

--- number 37.py
+++ number 37.py
@@ -1,5 +1,5 @@
 def ShortInfo (name, age, addr ):
-    print("My name is %s:" % name)
+    print("My name is %s" % name)
     print("I am: %d" % age, "years old")
     print("I live on %s" % addr, "street")
 
---
+++ number 48.py
@@ -0,0 +1,11 @@
+a = 48
+b = 42.1
+c = 23.1134
+
+print(int(b))
+print(int(c))
+
+lStr = "127"
+print(4 * lStr)
+print(4 * int(lStr))
+
