0dd2a7059ba4638f926b47efa8767c206bb841b6
tom3108 <t.madziara@gmail.com>
2020-04-27 22:25:50 +0200

    Kodolamacz task 2 ver2

---
+++ Kodolamacz 2 ver2.py
@@ -0,0 +1,34 @@
+parametr= True
+
+while parametr:
+    print("Podaj w oddzielnych wierszach liczbę, operację matematyczną: +,-,*,/,%, a następnie kolejną liczbę:")
+    liczba1 = float(input("Wprowadź pierwszą liczbę:"))
+    operacja = input("Wprowadź operację:")
+    liczba2 = float(input("Wprowadź drugą liczbę:"))
+
+    if operacja == "+":
+        wynik = liczba1 + liczba2
+        print("Twój wynik to:", wynik)
+    elif operacja == "-":
+        wynik = liczba1 - liczba2
+        print("Twój wynik to:", wynik)
+    elif operacja == "*":
+        wynik = liczba1 * liczba2
+        print("Twój wynik to:", wynik)
+    elif operacja == "/":
+        wynik = liczba1 / liczba2
+        print("Twój wynik to:", wynik)
+    elif operacja == "%":
+        wynik = liczba1 % liczba2
+        print("Twój wynik to:", wynik)
+    else:
+        print("Wprowadź poprawną operację")
+        continue
+
+    print("Chcesz wykonać operację ponowanie? Wpisz t/n")
+    again = input("Wykonać obliczenia ponownie?")
+
+    if again == "t":
+        parametr = True
+    else:
+        parametr = False
